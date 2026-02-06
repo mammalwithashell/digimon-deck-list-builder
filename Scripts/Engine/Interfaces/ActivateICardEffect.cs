16ee698a0e9393f33bf09bd92998db848ba9596c
james <james.peters97@outlook.com>
2026-02-05 20:32:59 -0600

    wip

--- Scripts/Engine/Interfaces/ActivateICardEffect.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-
-public interface ActivateICardEffect
-{
-    Permanent PermanentWhenTriggered { get; set; }
-    CardSource TopCardWhenTriggered { get; set; }
-    IEnumerator Activate(Hashtable hash);
-}
