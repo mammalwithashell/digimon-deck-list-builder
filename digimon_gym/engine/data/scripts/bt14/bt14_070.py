a3a28b466ea2e98fde8180def7c1bfe6de8f4a1d
james <james.peters97@outlook.com>
2026-02-27 03:43:18 -0600

    AI set-run autofix BT14-070

--- digimon_gym/engine/data/scripts/bt14/bt14_070.py
+++ digimon_gym/engine/data/scripts/bt14/bt14_070.py
@@ -13,7 +13,6 @@ class BT14_070(CardScript):
     def get_card_effects(self, card: 'CardSource') -> List['ICardEffect']:
         effects = []
 
-        # Timing: EffectTiming.OnDiscardHand
         # [Your Turn][Once Per Turn] When one of your effects trashes a card in your hand, gain 1 memory.
         effect0 = ICardEffect()
         effect0.set_effect_name("BT14-070 Memory +1")
